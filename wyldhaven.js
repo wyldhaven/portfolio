e258fcf0b40cbcc0c66bef1ddadfcbfbe0b350c8
graysonrandall <40443785+graysonrandall@users.noreply.github.com>
2018-06-29 20:58:35 -0500

    Update wyldhaven.js

--- wyldhaven.js
+++ wyldhaven.js
@@ -18,6 +18,41 @@ $(document).ready(function() {
     });
 });
 
+
+
+var setupValues = function() {
+  scrollArrow = document.querySelector('.scroll');
+  scrollCopy = document.querySelector('.arrow');
+};
+
+var animateIntro = function() {
+    $(".scroll").delay(0).animate({
+        opacity: 0
+    }, 3000, function() {
+        $(".scroll").animate({
+            opacity: 1,
+        }, 3000);
+    });
+
+    $(".arrow").delay(0).animate({
+        opacity: 0
+    }, 3000, function() {
+        $(".arrow").animate({
+            opacity: 1,
+        }, 3000);
+    });
+
+    // $(".intro-copy").delay(3500).animate({
+    //     opacity: 1
+    // }, 2000, function() {
+    //     $(".intro-scroll").animate({
+    //         opacity: 1
+    //     }, 1000);
+    // });
+};
+
+
+
 WYLD.scrolling = {
         init: init
     };
